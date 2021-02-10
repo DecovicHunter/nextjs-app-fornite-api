4a00f958a7036a1c08dd07f3e53c0de2e9706b4f
DecovicHunter <48398993+DecovicHunter@users.noreply.github.com>
2021-02-11 01:44:54 +0800

    Update [id].tsx

--- pages/item/[id].tsx
+++ pages/item/[id].tsx
@@ -4,27 +4,25 @@ import Link from "next/link";
 import fs from "fs";
 
 import { fetchData } from "../../dataFetch";
-import { Item as item } from "../../types";
+import { Item } from "../../types";
 import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
 import Loading from "../../components/Loading";
 
 interface IItemProps {
-  data: item;
+  item: Item;
 }
 
-const Item: React.FC<IItemProps> = ({ data }) => {
+const ItemDetails: React.FC<IItemProps> = ({ data }) => {
   if (!data) {
     return <Loading />;
   }
   const router = useRouter();
-  const filledstar = Math.round(data?.item?.ratings.avgStars);
-  const rating = Array.from({ length: filledstar }, (_, index) => {
-    return index;
-  });
+  const filledstar = Math.round(item?.item?.ratings.avgStars);
+  const ratings = Array.from({ length: filledstar }, (_, index) => index);
 
   return (
     <div className="md:p-12 bg-gray-100 min-h-screen ">
-      <div key={data?.itemId} className=" h-full relative">
+      <div key={item?.itemId} className=" h-full relative">
         <a
           onClick={() => router.back()}
           className="inline-block absolute md:top-5 md:left-5  top-2 left-2 font-bold p-2 text-gray-100 cursor-pointer"
@@ -33,44 +31,44 @@ const Item: React.FC<IItemProps> = ({ data }) => {
         </a>
         <div className="bg-gray-800 rounded-xl  mx-2 overflow-hidden flex flex-1 justify-center">
           <img
-            src={data.item?.images?.icon}
+            src={item.item?.images?.icon}
             className="object-cover "
-            alt={data.item.description}
+            alt={itemitem.item.description}
           />
         </div>
         <div className="p-4 flex-col  h-full">
-          <h1 className="text-3xl font-bold my-5  ">{data.item.name}</h1>
+          <h1 className="text-3xl font-bold my-5  ">{item.item.name}</h1>
           <div>
             <p className="inline font-bold text-lg">Type:</p>
-            <p className="inline">{data.item.type}</p>
+            <p className="inline">{item.item.type}</p>
           </div>
           <div>
             <p className="inline font-bold text-lg">Description:</p>{" "}
             <p className="text-gray-800  text-sm inline-block ">
-              {data.item.description}
+              {item.item.description}
             </p>
           </div>
           <div>
             <p className="inline font-bold text-lg">Cost:</p>
             {"  "}
-            <p className="text-gray-800  text-sm inline">{data.item.cost}</p>
+            <p className="text-gray-800  text-sm inline">{item.item.cost}</p>
           </div>
           <div>
             <p className="inline font-bold text-lg">Rarity:</p>{" "}
             <p className="text-gray-800  text-sm inline-block ">
-              {data.item.rarity}
+              {item.item.rarity}
             </p>
           </div>
           <div>
             <p className="inline font-bold text-lg">Series:</p>{" "}
             <p className="text-gray-800  text-sm inline-block ">
-              {data.item.series}
+              {item.item.series}
             </p>
           </div>
           <div className="flex text-center align-middle">
             <p className="inline font-bold text-lg">Stars: </p>{" "}
-            {rating?.map((_, index) => (
-              <img key={index} src="/icons/star.svg" width="20" />
+            {ratings?.map((vaue) => (
+              <img key={value} src="/icons/star.svg" width="20" />
             ))}
             {"  "}
             <p className="inline-block">
@@ -83,50 +81,34 @@ const Item: React.FC<IItemProps> = ({ data }) => {
   );
 };
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const itemId = context.params.id;
-//   let data: item;
-//   try {
-//     data = await fetchData("/item/get?id=" + itemId);
-//   } catch (error) {
-//     // console.log(error);
-//   }
-
-//   return {
-//     props: { data },
-//   };
-// };
-
 export const getStaticProps: GetStaticProps = async (context) => {
   const itemId = context.params.id;
-  let data: item;
+  let item: item;
   try {
     // data = await fetchData("/item/get?id=" + itemId);
     let buffer = fs.readFileSync("Data.json");
-    const datas = JSON.parse(await buffer.toString("utf-8"));
-    data = datas.filter((item) => item.itemId == itemId)[0];
+    const items = JSON.parse(await buffer.toString("utf-8"));
+    item = items.filter((item) => item.itemId == itemId)[0];
   } catch (error) {
-    console.log(error.message);
+    //console.log(error);
   }
 
   return {
-    props: { data },
+    props: { item },
   };
 };
 
-export const getStaticPaths: GetStaticPaths<{ id: string }> = async (
-  context
-) => {
-  let data: item[];
+export const getStaticPaths: GetStaticPaths = async () => {
+  let items: item[];
 
   try {
     let buffer = fs.readFileSync("Data.json");
-    data = JSON.parse(await buffer.toString("utf-8"));
+    items = JSON.parse(await buffer.toString("utf-8"));
   } catch (error) {
-    console.log(error);
+   // console.log(error);
   }
 
-  const paths = data.map((item) => ({
+  const paths = items.map((item) => ({
     params: { id: item.itemId.toString() },
   }));
 
@@ -136,4 +118,4 @@ export const getStaticPaths: GetStaticPaths<{ id: string }> = async (
   };
 };
 
-export default Item;
+export default ItemDetails;
