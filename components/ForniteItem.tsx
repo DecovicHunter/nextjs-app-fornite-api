53d1e99f9f5d9c728e3a62f6d15f1eab0e873be8
DecovicHunter <48398993+DecovicHunter@users.noreply.github.com>
2021-02-11 01:49:42 +0800

    Update ForniteItem.tsx

--- components/ForniteItem.tsx
+++ components/ForniteItem.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Link from "next/link
 import { Item } from "../types";
 
 interface IForniteItemProps {
@@ -7,12 +8,12 @@ interface IForniteItemProps {
 
 const ForniteItem: React.FC<IForniteItemProps> = ({ item }) => {
   const filledstar = Math.round(item.item.ratings.avgStars);
-  const rating = Array.from({ length: filledstar }, (_, index) => {
-    return index;
-  });
+  const rating = Array.from({ length: filledstar }, (_, index) => index);
 
   return (
-    <div key={item.itemId} className="">
+     <Link href={"./item/" + item.itemId}>
+      <a className="block  bg-gray-800 rounded-2xl m-2 h-full mx-4  md:w-1/4 hover:bg-gray-700 hover:z-10 transform hover:scale-105 transition-all ">
+      <div key={item.itemId} className="">
       <div className="w-full overflow-hidden" style={{ minHeight: 150 }}>
         <img
           src={item.item?.images?.icon}
@@ -29,12 +30,14 @@ const ForniteItem: React.FC<IForniteItemProps> = ({ item }) => {
         </p>
         <p className="text-gray-300">{item.item.ratings.avgStars}</p>
         <div className="flex">
-          {rating?.map((_, index) => (
-            <img key={index} src="/icons/star.svg" width="20" />
+          {rating?.map((value) => (
+            <img key={value} src="/icons/star.svg" width="20" />
           ))}
         </div>
       </div>
-    </div>
+      </div>
+     </a>
+    </Link>
   );
 };
 export default ForniteItem;
